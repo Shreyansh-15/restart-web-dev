ed0f80c009b99928f2cadfed1111ef940b2b8c32
Shreyansh Gupta <05shreyansh@gmail.com>
2026-02-01 00:29:06 +0530

    Day 6: Add live clock with start/stop using JavaScript

--- README.md
+++ README.md
@@ -13,3 +13,29 @@
   - Button click counter using JavaScript
   - Displayed current time dynamically
   - Practiced basic DOM manipulation and events
+- Day 6: JavaScript DOM & Live Clock ⏰
+
+## What I worked on
+- Practiced JavaScript DOM manipulation
+- Implemented a live digital clock using `setInterval`
+- Added Start and Stop functionality for the clock
+- Learned how to enable/disable buttons using JavaScript
+- Practiced handling button click events
+
+## Concepts Used
+- `document.getElementById()`
+- `addEventListener()`
+- `setInterval()` and `clearInterval()`
+- JavaScript Date object
+- Updating HTML using `textContent`
+- Using variables to control program state
+
+## What I learned
+- Why storing `setInterval` in a variable is important
+- How multiple intervals can cause bugs if not handled properly
+- How JavaScript interacts with HTML elements dynamically
+- How to write cleaner and more controlled event-based code
+
+## Status
+Completed ✅  
+Will improve styling and add more features later.
--- WEB _DEV/script.js
+++ WEB _DEV/script.js
@@ -4,11 +4,34 @@ const Button = document.getElementById("myBtn");
 const msg= document.getElementById("message");
 const timePara = document.getElementById("time");
 
+let clockInterval = null;
+const clock = document.getElementById("liveClock");
+const startBtn = document.getElementById("startClock");
+const stopBtn = document.getElementById("stopClock");
+
+startBtn.addEventListener("click", () =>{
+    if (!clockInterval) {
+        clockInterval = setInterval(() =>{
+            clock.textContent = new Date().toLocaleTimeString();
+    }, 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    }
+});
+    
+stopBtn.addEventListener("click", () => {
+    clearInterval(clockInterval);
+    clockInterval = null;
+
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+});
+
+
 Button.addEventListener("click", () =>{
     count++;
 
     const currentTime = new Date().toLocaleTimeString();
-    
     msg.textContent = `Button clicked ${count} times`;
     timePara.textContent = `Current Time: ${currentTime}`;
 });
